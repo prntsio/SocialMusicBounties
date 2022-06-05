387d3e9a61cc275d9e44cbedd17ffa4f1ef4717a
James Westbrook <james.westbrook@coalfire.com>
2022-06-04 21:27:18 -0700

    pulled updated mintNft function

--- src/utils/livepeer.js
+++ src/utils/livepeer.js
@@ -18,12 +18,87 @@ export async function upload (file) {
   uploader.pickFile().then(f => console.log(f)).catch(console.log)
 }
 
+
+
+
+
 // file is optional, will open a file picker if not provided.
 export async function mintNft(file) {
   console.log('asdf')
   let asset = await minter.api.createAsset('My NFT', file, (progress => {
     console.log(progress)
   }));
+
+/*
+async function mintNft() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+
+    // Prompt user for account connections
+    await provider.send("eth_requestAccounts", []);
+    let ethereum = window.ethereum;
+    let chainId = window.ethereum.chainId;
+
+
+    const signer = provider.getSigner();
+    console.log("Account:", await signer.getAddress());
+    console.log(ethereum)
+    console.log(chainId);
+
+    const minter = new videonft.minter.FullMinter(apiOpts, { ethereum, chainId });
+    const mintBtn = document.getElementById('mint-btn');
+
+    console.log(minter);
+    let file = window.livepeer.files[0];
+    let title = document.getElementById("title").value || "My NFT";
+    // two vars below added by me; all appear correctly in console
+    //let artist = document.getElementById("arist").value || "Artist Name";
+    //let genre = document.getElementById("genre").value || "Genre";
+    console.log(title)
+    //console.log(artist)
+    //console.log(genre)
+    console.log(file)
+    console.log('create asset', minter.api.createAsset);
+    let progressBar = document.getElementById('progress-bar')
+    mintBtn.innerText = "Creating Mintable Video Asset..."
+
+    let asset = await minter.api.createAsset(title, file, (progress) => {
+        console.log('progress: ', progress)
+        progressBar.style.width = `${progress * 100 }%`;
+    });
+
+    mintBtn.innerText = 'Minting....'
+    console.log("asset", asset);
+    // // optional, optimizes the video for the NFT
+    asset = await minter.api.nftNormalize(asset);
+    console.log(asset)
+    const nftMetadata = {
+        description: 'Bounty submission video',
+        traits: { 
+            // these metadata values aren't the issue; code fails when commented out
+            'Artist Name': 'Almighty Zay'
+        }
+    };
+    console.log("metadata", nftMetadata)
+
+    const nftInfo = await minter.createNft({
+        name: title,
+        mint: { 
+            contractAddress: '0xe1A31a270082008ed2Ee736671CFb77144Bd532a', 
+            to: '0x3a8eBfCcC377b09216586871be539aBFF9aB8ABf' // TODO: Pull from ethers; not MVP
+        },
+        file,
+        nftMetadata
+    });
+
+    mintBtn.innerText = `Minted Video with ID ${nftInfo.tokenId}`;
+    console.log(`minted NFT on contract ${nftInfo.contractAddress} with ID ${nftInfo.tokenId}`);
+    return nftInfo;
+}
+*/
+
+
+
+
   // optional, optimizes the video for the NFT
   console.log('asset created')
   asset = await minter.api.nftNormalize(asset);
