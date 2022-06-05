8578e6176306c42c6b2eec2c4fd7ee5b697b9186
aminaura <aura.otsuka@gmail.com>
2022-06-04 19:39:47 -0700

    .

--- src/modules/dashboard/components/Cards.tsx
+++ src/modules/dashboard/components/Cards.tsx
@@ -4,15 +4,14 @@ import { Link } from "react-router-dom";
 import polylogo from "../../../images/polygon.png";
 import { Bounty } from '../../../repositories/list-bounties'
 import { MusicNFT } from "../../../repositories/list-music-NFT";
+import { formatDistance, format } from "date-fns"
 interface Props {
     width: number;
     bounties: Bounty[];
     music: MusicNFT[];
 }
 
-
 const BountyCards: React.FC<Props> = (props) => {
-    
     let w = ""
     if(props.width === 100){
         w ="justify-content-md-center my-5"
@@ -22,21 +21,17 @@ const BountyCards: React.FC<Props> = (props) => {
     }
 
 
-
     return (
         <>
             {props.music.map(music => <div key={music.id}>tokenURI: {music.tokenURI}</div>)}
             {props.bounties.map(bounty =>
                 <Container key={bounty.id} className={w}>
                     <Card>
-                        <Card.Header>{"user name"} {bounty.id}</Card.Header>
+                        <Card.Header>{"Owned by"} {bounty.sender}</Card.Header>
                         <Card.Body>
                             <Card.Title>{bounty.title}</Card.Title>
                             <Card.Text>
                                 {bounty.description}
-                            </Card.Text>
-                            <Card.Text>
-                                {"Created by"} {bounty.sender}
                             </Card.Text>
                             <Stack className="justify-content-end" direction="horizontal" gap={3}>
                                 <Card.Text className="mt-3"> <Image src={polylogo} /> {bounty.bountyPrice} </Card.Text>
@@ -44,6 +39,9 @@ const BountyCards: React.FC<Props> = (props) => {
                                     <Button variant="primary">View</Button>
                                 </Link>
                             </Stack>
+                            <Card.Text>
+                                {formatDistance(new Date(), new Date(Number(bounty.createdAt) * 1000))}
+                            </Card.Text>
                         </Card.Body>
                     </Card>
                 </Container>
