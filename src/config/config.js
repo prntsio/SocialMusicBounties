aa8675dcc3abc94a0f9171b88148cb3cb25ee818
jonathanng1122 <jonathanng1122@gmail.com>
2022-06-05 01:15:56 -0500

    f

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,14 @@
-export const configProd = {
+const configProdTest = {
   rpcUrl: 'https://polygon-mainnet.g.alchemy.com/v2/bdC_I5MH1DESDMZoehzrdPjOeGMCmYih',
-  address: '',
+  address: '0xe1A31a270082008ed2Ee736671CFb77144Bd532a',
+  chainId: 137,
+  nftSubgraph: '',
+  
+}
+
+const configProd = {
+  rpcUrl: 'https://polygon-mainnet.g.alchemy.com/v2/bdC_I5MH1DESDMZoehzrdPjOeGMCmYih',
+  address: '0xe1A31a270082008ed2Ee736671CFb77144Bd532a',
   chainId: 137
 }
 
@@ -10,4 +18,4 @@ const configTest = {
   chainId: 80001,//polygon mumbai
 }
 
-export default configTest
+export default configProdTest
